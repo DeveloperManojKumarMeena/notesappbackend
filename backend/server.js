9c90b75d247af0f88f22bc739562f9dc6a2cbd0c
DeveloperManojKumarMeena <manojkumarmeena1996@outlook.com>
2025-11-05 07:55:22 +0530

    all rest api use

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,30 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 let notes = []
-app.get('/',(req,res)=>{
-  res.send('Hello World!');
-});
+
+app.get('/notes',(req,res)=>{
+  res.json(notes);
+})
 
 app.post('/notes',(req,res)=>{
-    const note = req.body;
-    notes.push(note);
-    console.log('Note added:', notes);
-  res.send('Note created!' + JSON.stringify(notes));
-});
+  notes.push(req.body);
+  res.status(201).send('Note added');
+})
+
+app.patch('/notes/:index',(req,res)=>{
+  const index = req.params.index; 
+  notes[index] = req.body;
+  res.send('Note updated');
+})
+
+app.delete('/notes/:index',(req,res)=>{
+  const index = req.params.index; 
+  notes.splice(index,1);
+  res.send('Note deleted');
+})
+
+
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
